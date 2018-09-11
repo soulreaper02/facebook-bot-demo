322ce9a79f2d663babfeab32a6f47a769017e945
Amit Kumar <slayer9191@gmail.com>
2018-09-11 15:14:05 +0530

    code push

---
+++ routes/webhook_verify.js
@@ -0,0 +1,35 @@
+const processPostback = require('../processes/postback');
+const processMessage = require('../processes/messages');
+
+module.exports = function(app, chalk){
+  app.get('/webhook', function(req, res) {
+    if (req.query['hub.verify_token'] === process.env.VERIFY_TOKEN){
+       console.log('webhook verified');
+       res.status(200).send(req.query['hub.challenge']);
+    } else {
+        console.error('verification failed. Token mismatch.');
+        res.sendStatus(403);
+     }
+  });
+  
+  app.post('/webhook', function(req, res) {
+    //checking for page subscription.
+    if (req.body.object === 'page'){
+       
+       /* Iterate over each entry, there can be multiple entries 
+       if callbacks are batched. */
+       req.body.entry.forEach(function(entry) {
+       // Iterate over each messaging event
+          entry.messaging.forEach(function(event) {
+          console.log(event);
+          if (event.postback){
+             processPostback(event);
+          } else if (event.message){
+             processMessage(event);
+          }
+      });
+    });
+    res.sendStatus(200);
+   }
+  });
+}
---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const morgan = require('morgan ');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+
+// app configuration
+app.set('port', (process.env.PORT || 3000));
+
+
+// setup our express application
+app.use(morgan('dev')); // log every request to the console.
+app.use(bodyParser.urlencoded({ extended:false }));
+app.use(bodyParser.json()); 
+
+
+// app routes
+require('./process/webhook_verify')(app);
+
+// warming up the engines !! setta !! go !!!.
+app.listen(app.get('port'), function() {
+  const url = 'http://localhost:' + app.set('port');
+  console.log('Application running on port: ', app.get('port'));
+});
