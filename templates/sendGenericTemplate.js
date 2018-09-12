bedea6e4b9a08a1a634643c20eeb713292b9e18f
Amit Kumar <slayer9191@gmail.com>
2018-09-12 16:41:35 +0530

    final

--- templates/sendGenericTemplate.js
+++ templates/sendGenericTemplate.js
@@ -1,6 +1,5 @@
 const request = require('request');
 
 module.exports = function sendGenericTemplate(recipientId, messageData) {
-    const data = JSON.parse(messageData);
-    console.log(data);
+    console.log(messageData);
 }
